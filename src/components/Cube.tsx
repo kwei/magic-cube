a77908f7c907a55b757c5ecadbb28f2f7da024b1
kw <a0979597291@gmail.com>
2022-03-11 22:03:49 +0800

    circular cube

--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -4,26 +4,64 @@ import styles from './cube.module.css';
 function Cube() {
     let cubeRef = useRef<HTMLDivElement>(null);
     var currentClass = useRef<string>('');
-    function dragging(e: React.DragEvent<HTMLDivElement>){ //only for div element's onDrag event
+    var currentFace = useRef<string>('');
+    var horizontalFaceIndex = useRef<Array<string>>(['front', 'right', 'back', 'left']);
+    var verticalFaceIndex = useRef<Array<string>>(['front', 'bottom', 'back', 'top']);
+    
+    function turn(e: React.DragEvent<HTMLDivElement>){ //only for div element's onDrag event
         let cubeDom = cubeRef.current;
         if ( cubeDom == null) {
             return;
         }
         e = e || window.event;
-        // console.log("X: " + e.pageX + " Y: " + e.pageY);
-        let direct = ""
+
+        currentFace.current = 'front';
+        if ( currentClass.current != '') {
+            currentFace.current = String(styles[currentClass.current]).split("-")[1].split("__")[0];
+        }
+        console.log("Current face: ", currentFace.current);
+        let direct = currentFace.current
+
         if(e.pageX > 300) {
-            direct = "left"
+            // direct = "left"
+            let lastElement = horizontalFaceIndex.current[3]
+            horizontalFaceIndex.current.pop();
+            horizontalFaceIndex.current.unshift(lastElement);
+            verticalFaceIndex.current[0] = horizontalFaceIndex.current[0];
+            verticalFaceIndex.current[2] = horizontalFaceIndex.current[2];
+            direct = horizontalFaceIndex.current[0];
         }
         else if (e.pageX < 90) {
-            direct = "right"
+            // direct = "right"
+            let firstElement = horizontalFaceIndex.current[0];
+            horizontalFaceIndex.current.shift();
+            horizontalFaceIndex.current.push(firstElement);
+            verticalFaceIndex.current[0] = horizontalFaceIndex.current[0];
+            verticalFaceIndex.current[2] = horizontalFaceIndex.current[2];
+            direct = horizontalFaceIndex.current[0];
         }
         else if (e.pageY < 80) {
-            direct = "bottom"
+            // direct = "bottom"
+            let firstElement = verticalFaceIndex.current[0];
+            verticalFaceIndex.current.shift();
+            verticalFaceIndex.current.push(firstElement);
+            horizontalFaceIndex.current[0] = verticalFaceIndex.current[0];
+            horizontalFaceIndex.current[2] = verticalFaceIndex.current[2];
+            direct = verticalFaceIndex.current[0];
         }
         else if (e.pageY > 285) {
             direct = "top"
+            let lastElement = verticalFaceIndex.current[3]
+            verticalFaceIndex.current.pop();
+            verticalFaceIndex.current.unshift(lastElement);
+            horizontalFaceIndex.current[0] = verticalFaceIndex.current[0];
+            horizontalFaceIndex.current[2] = verticalFaceIndex.current[2];
+            direct = verticalFaceIndex.current[0];
         }
+
+        console.log("horizontalFaceIndex: ", horizontalFaceIndex.current);
+        console.log("verticalFaceIndex: ", verticalFaceIndex.current);
+
         let showClass = 'show-' + direct;
         if ( currentClass ) {
             cubeDom.classList.remove( styles[currentClass.current] );
@@ -35,16 +73,17 @@ function Cube() {
     return (
         <div className={`${styles["scene"]}`}
             onDragOver={(e)=>{e.preventDefault();}}
+            onDrop={turn}
             >
 			<div ref={cubeRef} className={`${styles["cube"]}`} 
-                onDrag={dragging} draggable="true"
+                draggable="true"
                 >
-				<div className={`${styles["cube__face"]} ${styles["cube__face--front"]}`}></div>
-                <div className={`${styles["cube__face"]} ${styles["cube__face--back"]}`}></div>
-                <div className={`${styles["cube__face"]} ${styles["cube__face--right"]}`}></div>
-                <div className={`${styles["cube__face"]} ${styles["cube__face--left"]}`}></div>
-                <div className={`${styles["cube__face"]} ${styles["cube__face--top"]}`}></div>
-                <div className={`${styles["cube__face"]} ${styles["cube__face--bottom"]}`}></div>
+				<div className={`${styles["cube__face"]} ${styles["cube__face--front"]}`}>front</div>
+                <div className={`${styles["cube__face"]} ${styles["cube__face--back"]}`}>back</div>
+                <div className={`${styles["cube__face"]} ${styles["cube__face--right"]}`}>right</div>
+                <div className={`${styles["cube__face"]} ${styles["cube__face--left"]}`}>left</div>
+                <div className={`${styles["cube__face"]} ${styles["cube__face--top"]}`}>top</div>
+                <div className={`${styles["cube__face"]} ${styles["cube__face--bottom"]}`}>bottom</div>
 			</div>
 		</div>
     );
